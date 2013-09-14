98e857de5c4028446d5c89b3b12cdbf0dda2a58b
Xiulan12 <xiulan@Xiulans-MacBook-Pro.local>
2013-09-14 15:50:00 -0400

    change to set00

--- set00:1.rkt
+++ set00:1.rkt
@@ -1,7 +1,14 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname |lab01 Ex1|) (read-case-sensitive #t) (teachpacks ((lib "Test1.rkt" "installed-teachpacks") (lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "Test1.rkt" "installed-teachpacks") (lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")))))
-; d->s: Number -> Number
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname set00:1) (read-case-sensitive #t) (teachpacks ((lib "Test1.rkt" "installed-teachpacks") (lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "Test1.rkt" "installed-teachpacks") (lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")))))
+; days->seconds: Number -> Number
 ; GIVEN: days in leap year
 ; RETURN: the seconds in a leap year
-(* 366 (* 24 (* 60 60)))
+
+(define (sec1 d)
+ (* d (* 24 (* 60 60))))
+
+(sec1 366)
+
+
+
---
+++ set00:1.rkt~
@@ -0,0 +1,7 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |lab01 Ex1|) (read-case-sensitive #t) (teachpacks ((lib "Test1.rkt" "installed-teachpacks") (lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "Test1.rkt" "installed-teachpacks") (lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")))))
+; d->s: Number -> Number
+; GIVEN: days in leap year
+; RETURN: the seconds in a leap year
+(* 366 (* 24 (* 60 60)))
